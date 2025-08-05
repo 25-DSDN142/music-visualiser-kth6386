a7d87dd50d69b7e9eac521d918e1a72dde66f06d
kth6386 <kth6386@gmail.com>
2025-08-05 12:03:19 +1200

    Update music_starter.js

--- music_starter.js
+++ music_starter.js
@@ -1,43 +1,41 @@
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  background(20)
-  textFont('Verdana'); // please use CSS safe fonts
-  rectMode(CENTER)
-  textSize(24);
-  
-   let bar_spacing = height / 10;
-   let bar_height = width / 12;
-   let bar_pos_x = width / 2;
- 
-// changes 
-   // vocal bar is red
-   fill(200, 0, 0);
-   rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-   fill(0);
-   text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
- 
-   // drum bar is green
-   fill(0, 200, 0);
-   rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-   fill(0);
-   text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
- 
-   // bass bar is blue
-   fill(50, 50, 240);
-   rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-   fill(0);
-   text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
- 
-   // other bar is white
-   fill(200, 200, 200);
-   rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-   fill(0);
-   text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-   fill(255, 255, 0);
- 
-   // display "words"
-   textAlign(CENTER);
-   textSize(vocal);
-   text(words, width/2, height/3);
+   background(80);
+
+   angleMode(DEGREES);
+
+   let midX = canvasWidth  / 2;
+   let midY = canvasHeight / 2;  
+
+   let sunSize = map (drum, 0, 100, 50, 400);
+   let strokeThickness = map (other, 0, 100, 10, 100);
+   let lineLength = map (vocal, 0, 100, 10, 200);
+
+   fill (219, 255, 161);
+   ellipse (midX, midY, sunSize);
+   stroke(255, 204, 0);
+   strokeWeight (strokeThickness);
+   
+   push ();
+
+   stroke(50, 168, 82);
+   strokeWeight (strokeThickness);
+   line (midX - lineLength, midY + lineLength, midX + lineLength, midY + lineLength);
+
+   pop ();
+
+
+
+
+//use websafe fonts
+
+
+
+
+
+
+
+
+
 }
